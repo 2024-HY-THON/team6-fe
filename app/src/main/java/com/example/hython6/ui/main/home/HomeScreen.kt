20537e33d4b0a6eeccb4c66876239557b76bcf2f
Sorongos <dosora9@gmail.com>
2024-11-29 03:11:17 +0900

    feat(HomeScreen): 홈 앱바 타이틀 설정
    LaunchEffect 사용해줘야지 recomposition이 일어나지 않음

--- app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
@@ -4,20 +4,23 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen() {
+fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
     Scaffold {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = Color.LightGray,
-        ){
-            Text(text = "홈페이지")
+        ) {
+            LaunchedEffect(Unit) {
+                appBarTitleSetter("안해도 돼")
+            }
         }
     }
 }
