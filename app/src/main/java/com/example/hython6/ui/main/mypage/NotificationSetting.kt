83f84958578866f22fbbdcadc0ef2727253dc7d0
Sorongos <dosora9@gmail.com>
2024-11-29 03:31:38 +0900

    feat(NotificationSetting): 상단 설명 추가

--- app/src/main/java/com/example/hython6/ui/main/mypage/NotificationSetting.kt
+++ app/src/main/java/com/example/hython6/ui/main/mypage/NotificationSetting.kt
@@ -1,23 +1,52 @@
 package com.example.hython6.ui.main.mypage
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
+@Preview
 fun NotificationSetting() {
-    Scaffold {
+    Scaffold(
+        containerColor = Color.White,
+    ) {
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = Color.LightGray,
-        ){
-            Text(text = "알림설정")
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = 15.dp,
+                ),
+            color = Color.White,
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Text(
+                    text = "랜덤은 아침, 점심, 저녁 중으로 가장 적합한 때에 랜덤으로 발송돼요 :)",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        lineHeight = 20.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF8E8E8E),
+                    )
+                )
+            }
+            
         }
     }
 }
