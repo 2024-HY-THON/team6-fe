b9b2accdb95858cae4bab36df33c09c085bcda5c
Sorongos <dosora9@gmail.com>
2024-11-29 02:17:36 +0900

    refactor(MainActivity): navController 선언
    MyPageScreen에서도 네비게이션 이용하기 위함

--- app/src/main/java/com/example/hython6/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/hython6/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hython6.ui.main
 
+import MyPageScreen
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -20,10 +21,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.hython6.data.HyThon6Destination
 import com.example.hython6.ui.main.home.HomeScreen
 import com.example.hython6.ui.main.list.ListScreen
-import com.example.hython6.ui.main.mypage.MyPageScreen
 import com.example.hython6.ui.theme.Blue
 import com.example.hython6.ui.theme.HyThon6Theme
 import com.example.hython6.ui.theme.Gray1
@@ -55,6 +58,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     HyThon6Theme {
+        val navController = rememberNavController()
         var currentDestination by remember { mutableStateOf(HyThon6Destination.HOME) }
         Scaffold(
             modifier = Modifier.fillMaxSize(),
@@ -89,10 +93,15 @@ fun App() {
                     .fillMaxSize()
                     .padding(innerPadding),
             ) {
+                NavHost(navController, startDestination = "home") {
+                    composable("home") { HomeScreen() }
+                    composable("list") { ListScreen() }
+                    composable("my_page_screen") { MyPageScreen() }
+                }
                 when (currentDestination) {
-                    HyThon6Destination.HOME -> HomeScreen()
-                    HyThon6Destination.LIST -> ListScreen()
-                    HyThon6Destination.MY_PAGE -> MyPageScreen()
+                    HyThon6Destination.HOME -> navController.navigate("home")
+                    HyThon6Destination.LIST -> navController.navigate("list")
+                    HyThon6Destination.MY_PAGE -> navController.navigate("my_page_screen")
                 }
             }
         }
