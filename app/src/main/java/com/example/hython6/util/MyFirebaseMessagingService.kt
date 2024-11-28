84030136f902acbd5992a2e93fcc7bce79ff1485
Sorongos <dosora9@gmail.com>
2024-11-28 18:37:03 +0900

    feat: 헤드업 알림 추가

--- app/src/main/java/com/example/hython6/util/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/hython6/util/MyFirebaseMessagingService.kt
@@ -2,8 +2,14 @@ package com.example.hython6.util
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.hython6.R
+import com.example.hython6.ui.main.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -12,21 +18,56 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-        val name = "알림 제목"
-        val descriptionText = "알림 설명입니다."
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val mChannel = NotificationChannel(getString(R.string.default_notification_channel_id), name, importance)
-        mChannel.description = descriptionText
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(mChannel)
-
-        val body = message.notification?.body ?: ""
-        val notificationBuilder = NotificationCompat.Builder(applicationContext, getString(R.string.default_notification_channel_id))
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(getString(R.string.app_name))
-            .setContentText(body)
-        notificationManager.notify(0, notificationBuilder.build())
+        message.notification?.let { notification ->
+            val title = notification.title ?: "Default title"
+            val body = notification.body ?: "Default body"
+            showNotification(title, body)
+        }
     }
+
+    private fun showNotification(title: String, body: String) {
+        createNotificationChannel()
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        val notificationBuilder = NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setContentIntent(pendingIntent)
+            .setFullScreenIntent(pendingIntent, true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(System.currentTimeMillis().toInt(), notificationBuilder.build())
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = getString(R.string.default_notification_channel_id)
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            if (notificationManager.getNotificationChannel(channelId) == null) {
+                val name = "습관 알림"
+                val descriptionText = "습관 알림 채널"
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                val channel = NotificationChannel(channelId, name, importance).apply {
+                    this.description = descriptionText
+                }
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+    }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
     }
