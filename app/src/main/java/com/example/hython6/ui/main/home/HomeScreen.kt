a7ceb2027ef5b31619dfb8833b494f5b63467403
Sorongos <dosora9@gmail.com>
2024-11-30 05:01:55 +0900

    feat: 상단 박스 두 개 생성

--- app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
@@ -1,25 +1,75 @@
 package com.example.hython6.ui.main.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
     Scaffold {
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = Color.LightGray,
+            modifier = Modifier
+                .fillMaxSize(),
+            color = Color.White,
         ) {
             LaunchedEffect(Unit) {
                 appBarTitleSetter("안해도 돼")
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
+                ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = Color(0xFFF7F7F7),
+                            shape = RoundedCornerShape(size = 20.dp)
+                        )
+                        .border(
+                            width = 0.8.dp,
+                            color = Color(0xFFDCDBDB),
+                            shape = RoundedCornerShape(size = 20.dp)
+                        )
+                        .height(150.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxSize()
+                    )
+                    Divider(
+                        color = Color(0xFFDCDBDB),
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(0.8.dp)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxSize()
+                    )
+                }
             }
         }
     }
