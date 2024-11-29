3335e061729c9886310bcee515f0111714dd85cd
Sorongos <dosora9@gmail.com>
2024-11-30 06:09:55 +0900

    feat: 홈 퍼블리싱 완
    디자인은 미룬이

--- app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
@@ -1,38 +1,37 @@
 package com.example.hython6.ui.main.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.platform.LocalConfiguration
+import coil.compose.rememberImagePainter
+import com.example.hython6.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    var imageRes by remember { mutableStateOf(R.drawable.ham_default) }
+    val coroutineScope = rememberCoroutineScope()
 
     Scaffold {
         Surface(
@@ -185,7 +184,14 @@ fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
                     Box(
                         modifier = Modifier
                             .weight(1f)
-                            .fillMaxSize(),
+                            .fillMaxSize()
+                            .clickable {
+                                coroutineScope.launch {
+                                    imageRes = R.drawable.ham_good
+                                    delay(2000)
+                                    imageRes = R.drawable.ham_default
+                                }
+                            },
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
@@ -203,7 +209,14 @@ fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
                     Box(
                         modifier = Modifier
                             .weight(1f)
-                            .fillMaxSize(),
+                            .fillMaxSize()
+                            .clickable {
+                                coroutineScope.launch {
+                                    imageRes = R.drawable.ham_good
+                                    delay(2000)
+                                    imageRes = R.drawable.ham_default
+                                }
+                            },
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
@@ -213,7 +226,14 @@ fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
                         )
                     }
                 }
-
+                Spacer(modifier = Modifier.height(12.dp))
+                Image(
+                    painter = rememberImagePainter(data = imageRes),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(screenHeight * 0.25f)
+                )
             }
         }
     }
