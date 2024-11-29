9daf0bad43c1c75898ab747c20a5b1056c2ebe1a
Sorongos <dosora9@gmail.com>
2024-11-30 05:05:32 +0900

    feat: 상단 박스에 더미 텍스트 추가

--- app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/hython6/ui/main/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,11 +17,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -38,7 +42,7 @@ fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(12.dp),
-                ) {
+            ) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -57,7 +61,26 @@ fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxSize()
-                    )
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(modifier = Modifier.weight(1f))
+                            Text(
+                                text = "까짓거 할게",
+                                fontSize = 16.sp,
+                                color = Color.Black
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Text(
+                                text = "0개!",
+                                fontSize = 16.sp,
+                                color = Color.Black
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                        }
+                    }
                     Divider(
                         color = Color(0xFFDCDBDB),
                         modifier = Modifier
@@ -68,7 +91,26 @@ fun HomeScreen(appBarTitleSetter: (String) -> Unit) {
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxSize()
-                    )
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(modifier = Modifier.weight(1f))
+                            Text(
+                                text = "줏대있게 안 할게:",
+                                fontSize = 16.sp,
+                                color = Color.Black
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Text(
+                                text = "0개!",
+                                fontSize = 16.sp,
+                                color = Color.Black
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                        }
+                    }
                 }
             }
         }
