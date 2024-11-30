9ed24f6990c5e2c944ca054342204d448a2c3a1d
sorongosdev <dosora9@gmail.com>
2024-11-30 13:50:23 +0900

    feat: 습관 오른쪽 버튼 누르면 시간 설정 다이얼로그 표출

--- app/src/main/java/com/example/hython6/ui/main/mypage/NotificationSetting.kt
+++ app/src/main/java/com/example/hython6/ui/main/mypage/NotificationSetting.kt
@@ -1,6 +1,5 @@
 package com.example.hython6.ui.main.mypage
 
-import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -20,10 +19,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
 @Composable
 fun NotificationSetting() {
-    val amPmOptions = listOf("AM", "PM")
+    val amPmOptions = listOf("오전", "오후")
     val hours = (1..12).toList()
     val minutes = (0..59).toList()
 
@@ -32,12 +33,15 @@ fun NotificationSetting() {
     var selectedMinute by remember { mutableStateOf(minutes[0]) }
     val context = LocalContext.current
 
+    var showDialog by remember { mutableStateOf(false) }
+
     Scaffold(
         containerColor = Color.White
-    ) {
+    ) { contentPadding ->
         Surface(
             modifier = Modifier
                 .fillMaxSize()
+                .padding(contentPadding)  // contentPadding 사용
                 .padding(top = 15.dp),
             color = Color.White,
         ) {
@@ -46,7 +50,6 @@ fun NotificationSetting() {
                     .fillMaxWidth()
                     .padding(12.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
             ) {
                 Row(
                     modifier = Modifier
@@ -55,27 +58,84 @@ fun NotificationSetting() {
                     horizontalArrangement = Arrangement.SpaceEvenly,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    TimeDial(
-                        options = amPmOptions,
-                        selectedOption = selectedAmPm,
-                        onOptionSelected = { selectedAmPm = it }
-                    )
-                    TimeDial(
-                        options = hours,
-                        selectedOption = selectedHour,
-                        onOptionSelected = { selectedHour = it }
-                    )
-                    TimeDial(
-                        options = minutes,
-                        selectedOption = selectedMinute,
-                        onOptionSelected = { selectedMinute = it }
-                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            var checked by remember { mutableStateOf(false) }
+                            Checkbox(
+                                checked = checked,
+                                onCheckedChange = { checked = it }
+                            )
+                            Text(
+                                text = "운동",
+                                style = TextStyle(
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black
+                                ),
+                                modifier = Modifier.padding(start = 8.dp)
+                            )
+                        }
+                        Button(onClick = { showDialog = true }) {
+                            Text(text = "랜덤")
+                        }
+                    }
                 }
-                Button(onClick = {
-                    Toast.makeText(context, "알림 시간이 설정되었습니다", Toast.LENGTH_SHORT).show()
-//                  //TODO: Save the selected time to the database
-                }) {
-                    Text("확인")
+            }
+        }
+    }
+
+    if (showDialog) {
+        Dialog(
+            onDismissRequest = { showDialog = false },
+            properties = DialogProperties(dismissOnClickOutside = true)
+        ) {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = Color.White,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Row(
+                        modifier = Modifier.padding(16.dp),
+                    ) {
+                        TimeDial(
+                            options = amPmOptions,
+                            selectedOption = selectedAmPm,
+                            onOptionSelected = { selectedAmPm = it }
+                        )
+                        TimeDial(
+                            options = hours,
+                            selectedOption = selectedHour,
+                            onOptionSelected = { selectedHour = it }
+                        )
+                        TimeDial(
+                            options = minutes,
+                            selectedOption = selectedMinute,
+                            onOptionSelected = { selectedMinute = it }
+                        )
+
+                    }
+                    Button(onClick = {
+                        showDialog = false
+                        val selectedTime = "${selectedAmPm} ${selectedHour}시 ${selectedMinute}분"
+                        Toast.makeText(context, "알림 시간: $selectedTime", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text("확인")
+                    }
                 }
             }
         }
@@ -86,12 +146,12 @@ fun NotificationSetting() {
 fun <T> TimeDial(
     options: List<T>,
     selectedOption: T,
-    onOptionSelected: (T) -> Unit
+    onOptionSelected: (T) -> Unit,
 ) {
     LazyColumn(
         modifier = Modifier
-            .width(60.dp)
-            .height(150.dp)
+            .width(80.dp)
+            .height(200.dp)
             .border(1.dp, Color.Gray, RoundedCornerShape(10.dp))
             .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
